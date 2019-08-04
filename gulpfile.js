34cda44d42590a84aa7e1665ec54b343ba088f18
ThisCode.Works <michal@thiscode.works>
2019-08-04 09:30:01 +0200

    - clean gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,15 @@
-var path = require('path'),
-  http = require('http'),
+  var 
+  path = require('path'),
   fs = require('fs'),
-
   gulp = require('gulp'),
-  gutil = require('gulp-util'),
-  markdown = require('gulp-markdown'),
-  gulpBump = require('gulp-bump'),
+  gutil = require('gulp-util'),  
   notify = require('gulp-notify'),
   plumber = require('gulp-plumber'),
-  watch = require('gulp-watch'),  
-  open = require('gulp-open'),
+  watch = require('gulp-watch'),
   sass = require('gulp-sass'),
   sassGlob = require('gulp-sass-glob'),
   rename = require('gulp-rename'),
   jshint = require('gulp-jshint'),
-  stylish = require('jshint-stylish'),
   concat = require('gulp-concat'),
   uglify = require('gulp-uglify'),
   uglifycss = require('gulp-uglifycss'),
@@ -24,49 +19,41 @@ var path = require('path'),
   sourcemaps = require('gulp-sourcemaps'),
   svgSymbols = require('gulp-svg-symbols'),
   embedSvg = require('gulp-embed-svg'),
-
-  eventStream = require('event-stream'),
-  hljs = require('highlight.js'),
-  express = require('express'),
-  connectLivereload = require('connect-livereload'),
   hogan = require('gulp-hogan'),
   moment = require('moment'),
-  del = require('del'),
   streamqueue = require('streamqueue'),
-  bump = require('./build/bump'),
-
-  args = require('minimist')(process.argv.slice(2)),
   browserSync = require('browser-sync').create(),
-
-  DEV = args.d || false,  
-
   pkg = require('./package.json'),
   build = require('./build/build.json');
 
-  // banner = hogan
-  // .compile(
-  //   ['/**',
-  //     ' * {{ pkg.name }} v{{ pkg.version }}',
-  //     ' * {{ pkg.author }} ~ {{ date }}',
-  //     ' */\n'
-  //   ].join('\n'))
-  // .render({
-  //   pkg: pkg,
-  //   date: moment().format("MMM Do YYYY")
-  // });
+// banner = hogan
+// .compile(
+//   ['/**',
+//     ' * {{ pkg.name }} v{{ pkg.version }}',
+//     ' * {{ pkg.author }} ~ {{ date }}',
+//     ' */\n'
+//   ].join('\n'))
+// .render({
+//   pkg: pkg,
+//   date: moment().format("MMM Do YYYY")
+// });
 
-gulp.task('pages', function() {    
+gulp.task('pages', function() {
   return gulp
-    .src(build.core.path.pages+'/*.hjs')    
-    .pipe(hogan({},null,'.html'))    
-    .pipe(rename({extname:'.html'}))  
-    .pipe(gulp.dest('./dist'))    
-    .on('end', function(){ gutil.log('Stránky vygenerovány') });
+    .src(build.core.path.pages + '/*.hjs')
+    .pipe(hogan({}, null, '.html'))
+    .pipe(rename({
+      extname: '.html'
+    }))
+    .pipe(gulp.dest('./dist'))
+    .on('end', function() {
+      gutil.log('Stránky vygenerovány')
+    });
 });
 
 gulp.task('styles-build', function() {
   return gulp
-    .src(build.core.path.styles+'/main.scss')
+    .src(build.core.path.styles + '/main.scss')
     .pipe(sassGlob())
     .pipe(plumber({
       errorHandler: function(error) {
@@ -78,9 +65,7 @@ gulp.task('styles-build', function() {
         this.emit('end');
       }
     }))
-    .pipe(sass({
-      compress: !DEV
-    }))
+    .pipe(sass({compress: true}))
     .pipe(uglifycss({
       "maxLineLen": 80,
       "uglyComments": true
@@ -92,7 +77,7 @@ gulp.task('styles-build', function() {
 
 gulp.task('styles', function() {
   return gulp
-    .src(build.core.path.styles+'/main.scss')
+    .src(build.core.path.styles + '/main.scss')
     .pipe(sassGlob())
     .pipe(plumber({
       errorHandler: function(error) {
@@ -106,9 +91,7 @@ gulp.task('styles', function() {
     }))
     .pipe(sourcemaps.init())
     .pipe(sourcemaps.identityMap())
-    .pipe(sass({
-      compress: !DEV
-    }))
+    .pipe(sass({compress: true}))
     .pipe(uglifycss({
       "maxLineLen": 80,
       "uglyComments": true
@@ -121,9 +104,9 @@ gulp.task('styles', function() {
 
 gulp.task('scripts', function() {
 
-  var libraries = require(build.core.path.scripts+'/libraries.json' );
-  var scripts = require(build.core.path.scripts+'/scripts.json' );
-    
+  var libraries = require(build.core.path.scripts + '/libraries.json');
+  var scripts = require(build.core.path.scripts + '/scripts.json');
+
   return streamqueue({
         objectMode: true
       },
@@ -135,7 +118,7 @@ gulp.task('scripts', function() {
     }))
     .pipe(jshint())
     .pipe(jshint.reporter('default'))
-    .pipe(gulpif(!DEV, uglify()))
+    .pipe(gulpif(true, uglify()))
     //.pipe(header(banner))
     .pipe(concat('index.js'))
     .pipe(gulp.dest(build.core.dist.scripts));
@@ -150,18 +133,18 @@ gulp.task('copy', function() {
 });
 
 gulp.task('browser-sync', function(done) {
-    browserSync.init({                  
-      injectChanges: true,
-      server: {
-          baseDir: "./dist",
-          port: 9021
-      }
-    });
-    done();
+  browserSync.init({
+    injectChanges: true,
+    server: {
+      baseDir: "./dist",
+      port: 9021
+    }
+  });
+  done();
 });
 
 gulp.task('sprites-svg', function() {
-  return gulp.src(build.core.path.assets+'/symbols/*.svg')
+  return gulp.src(build.core.path.assets + '/symbols/*.svg')
     .pipe(svgSymbols({
       templates: [
         `default-svg`,
@@ -179,19 +162,23 @@ gulp.task('sprites-sass', function() {
       ],
     }))
     .pipe(gulp.dest('src/core/styles/helpers'));
-    
+
   gutil.log('Symboly vygenerovány');
-  
-  gulp.src("src/core/styles/helpers/svg-symbols.scss",{ allowEmpty: true })
+
+  gulp.src("src/core/styles/helpers/svg-symbols.scss", {
+      allowEmpty: true
+    })
     .pipe(rename("src/core/styles/helpers/_icons.scss"))
     .pipe(gulp.dest("src/core/styles/helpers/"));
-    
-  gulp.src("dist/assets/svg-symbols.svg",{ allowEmpty: true })
+
+  gulp.src("dist/assets/svg-symbols.svg", {
+      allowEmpty: true
+    })
     .pipe(rename("src/pages/partials/icons.hjs"))
     .pipe(gulp.dest("src/pages/partials/"));
-    
+
   gutil.log('Přejmenovány symboly na ikony.');
-  
+
   return true;
 });
 
@@ -207,18 +194,24 @@ gulp.task('embedSvgs', function() {
 gulp.task('build', gulp.series('pages', 'styles', 'scripts', 'sprites-svg', 'copy', 'embedSvgs'));
 gulp.task('build-production', gulp.series('pages', 'styles-build', 'scripts', 'sprites-svg', 'sprites-sass', 'copy', 'embedSvgs'));
 
-gulp.task('watch', gulp.series('browser-sync','build', function() {      
-  function reload(file){    
-    setTimeout(function(){ lr.changed({body: {files: ["styles.css","index.js"]}});},5000);
-    
-    gutil.log('Změna v souboru', gutil.colors.magenta(file));      
+gulp.task('watch', gulp.series('browser-sync', 'build', function() {
+  function reload(file) {
+    setTimeout(function() {
+      lr.changed({
+        body: {
+          files: ["styles.css", "index.js"]
+        }
+      });
+    }, 5000);
+
+    gutil.log('Změna v souboru', gutil.colors.magenta(file));
     gutil.log(gutil.colors.green.bold.underline('Hlídám změnu v souborech ...'));
   }
-  
-  gulp.watch([path.join(build.core.path.pages, '*.hjs'),path.join(build.core.path.pages, '/**/*.hjs')], gulp.series('pages')).on('change', browserSync.reload);
-  gulp.watch(build.core.path.styles+'/**/*.scss', gulp.series('styles')).on('change', browserSync.reload);
-  gulp.watch(build.core.path.scripts+"/*.js", gulp.series('scripts')).on('change', browserSync.reload);
-  gulp.watch(build.core.path.assets+"/**", gulp.series('copy'));
-  gulp.watch(build.core.path.assets+"/symbols/*.svg", gulp.series('sprites-svg', 'sprites-sass', 'embedSvgs'));
-  gutil.log(gutil.colors.green.bold.underline('Hlídám změnu v souborech ...'));  
+
+  gulp.watch([path.join(build.core.path.pages, '*.hjs'), path.join(build.core.path.pages, '/**/*.hjs')], gulp.series('pages')).on('change', browserSync.reload);
+  gulp.watch(build.core.path.styles + '/**/*.scss', gulp.series('styles')).on('change', browserSync.reload);
+  gulp.watch(build.core.path.scripts + "/*.js", gulp.series('scripts')).on('change', browserSync.reload);
+  gulp.watch(build.core.path.assets + "/**", gulp.series('copy'));
+  gulp.watch(build.core.path.assets + "/symbols/*.svg", gulp.series('sprites-svg', 'sprites-sass', 'embedSvgs'));
+  gutil.log(gutil.colors.green.bold.underline('Hlídám změnu v souborech ...'));
 }));
